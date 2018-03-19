17277efd06e09f3dbb5355e854b7f56e97495f18
Eldar Aliyev <eldar.aliyev8@gmail.com>
2018-03-19 17:12:23 +0200

    Failed async tests

--- wait-for-value.test.js
+++ wait-for-value.test.js
@@ -1,22 +1,25 @@
 const waitForValue = require('./wait-for-value');
 
+const shortTimeout = 1000;
+const longTimeout = 6000;
+
 describe('Jest async experiment', async() => {
-    it('First', async() => {
+    it('Long timeout, more than default 5000', async() => {
         expect.assertions(1);
         const testValue = 'one';
-        const result = await waitForValue(1000, testValue);
+        const result = await waitForValue(longTimeout, testValue);
         expect(result).toMatchSnapshot();
     });
-    it('Second', async() => {
+    it('Short timeout', async() => {
         expect.assertions(1);
         const testValue = 'two';
-        const result = await waitForValue(1000, testValue);
+        const result = await waitForValue(shortTimeout, testValue);
         expect(result).toMatchSnapshot();
     });
-    it('Third', async() => {
+    it('Short timeout one more time', async() => {
         expect.assertions(1);
         const testValue = 'three';
-        const result = await waitForValue(1000, testValue);
+        const result = await waitForValue(shortTimeout, testValue);
         expect(result).toMatchSnapshot();
     });
 });
